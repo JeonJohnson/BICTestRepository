4cadaf4d0621b720ec6c1fb232bcef6b9a5d8686
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2023-02-13 23:18:59 +0900

    update unit 2

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -11,44 +11,56 @@ public struct UnitState
 
     public float moveSpd;
 
+    public float viewRange;
+
 }
 
 public class Unit : MonoBehaviour
 {
-    public UnitState state;
+	public UnitState state;
 
-    public Vector3 destPos;
-    public Vector3 dir;
-    public float dist;
+	public Vector3 destPos;
+	public Vector3 dir;
+	public float dist;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        destPos = transform.position;
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		destPos = transform.position;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
 
-        if (Input.GetMouseButtonDown(1))
+		if (Input.GetMouseButtonDown(1))
 		{
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Terrain")))
-            {
-                //GameObject tileObj = hit.collider.gameObject;
+			if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Terrain")))
+			{
+				//GameObject tileObj = hit.collider.gameObject;
 
-                destPos = hit.collider.gameObject.transform.position;
-            }
-        }
+				destPos = hit.collider.gameObject.transform.position;
+			}
+		}
 
-        dir = (destPos - transform.position).normalized;
-        dist = Vector3.Distance(destPos, transform.position);
-        if (dist > 0.1f)
-        { transform.position += dir * Time.deltaTime * state.moveSpd; }
+		dir = (destPos - transform.position).normalized;
+		dist = Vector3.Distance(destPos, transform.position);
+		if (dist > 0.1f)
+		{ transform.position += dir * Time.deltaTime * state.moveSpd; }
 
 	}
+
+	private void OnDrawGizmos()
+	{
+		Color temp = Color.green;
+		temp.a = 0.4f;
+		Gizmos.color = temp;
+
+		Gizmos.DrawSphere(transform.position, state.viewRange);
+	}
 }
+
